d436b3a0160966e202b00e0cf414e6e8be2f8f74
Elvisrrahmani <elvisrrahmani0@gmail.com>
2026-01-17 14:48:03 +0100

    bonjur php

---
+++ Moduli2/index.php
@@ -0,0 +1,24 @@
+<?php
+$school_Name = "Digital School";
+
+echo("Welcome to ".$school_Name)."<hr>";
+
+$numri = 10;
+
+echo "Numri eshte: ".$numri."<hr>";
+
+
+
+
+
+$the_String = "Hello World";
+
+echo strlen($the_String)."<hr>";
+
+echo str_word_count($the_String)."<hr>";
+
+echo str_replace("World","PHP",$the_String)."<hr>";
+
+echo strrev($the_String)."<hr>";
+
+?>
